d8ccf1260e6cdbf3135cdae9d2aea5a85dba82dc
Nick Ducca <lapplejack6@gmail.com>
2019-05-04 23:36:43 -0700

    commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("cheesecake burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheesecake burger", false);
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("Teriyaki Pineapple burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Teriyaki Pineapple burger", false);
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("The Burger Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Burger Burger", false);
